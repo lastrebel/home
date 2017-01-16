97a39ab11156ee517b8b3023604d83ad30637b13
“last.rebel” <valasek.brano@gmail.com>
2017-01-16 18:46:16 +0100

    test

--- menu.php
+++ menu.php
@@ -1,8 +1,5 @@
-<?php
-
 
 <a href="http://www.tutorialspoint.com/index.htm">Home</a> - 
 <a href="http://www.tutorialspoint.com/ebxml">ebXML</a> - 
 <a href="http://www.tutorialspoint.com/ajax">AJAX</a> - 
 <a href="http://www.tutorialspoint.com/perl">PERL</a> <br />
-?>
